f3cb27d42a4dea7cfa5c2f177eef4f002316bfd2
markoi95 <marko.ivanovic95@outlook.com>
2023-06-15 17:26:42 +0200

    db - promena imena baze

--- dbBroker.php
+++ dbBroker.php
@@ -2,7 +2,7 @@
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $host = "localhost:3308";
-$db = "rezervacije";
+$db = "sistemDB";
 $user = "root";
 $pass = "";
 
@@ -10,6 +10,6 @@ $conn = new mysqli($host,$user,$pass,$db);
 
 
 if ($conn->connect_errno){
-    exit("Nauspesna konekcija: greska> ".$conn->connect_error.", err kod>".$conn->connect_errno);
+    exit("Neuspesna konekcija: greska> ".$conn->connect_error.", err kod>".$conn->connect_errno);
 }
 ?>  
